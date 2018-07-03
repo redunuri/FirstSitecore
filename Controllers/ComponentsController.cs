8de57fb535a0fdf88c90fabf17205f82cbb2ebf3
Ravinder <39098619+redunuri@users.noreply.github.com>
2018-07-03 09:55:41 -0700

    just uploaded

---
+++ Controllers/ComponentsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sitecore;
+using Sitecore.Data.Items;
+using Sitecore.Mvc.Controllers;
+using Sitecore.Mvc.Presentation;
+
+namespace SitecoreDev.Website.Controllers
+{
+    public class ComponentsController : SitecoreController
+    {
+        //// GET: Components
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public ViewResult HeroSlider()
+        {
+            Item contentItem = null;
+            var database = Context.Database;
+            if (database != null)
+            {
+                if (!String.IsNullOrEmpty(
+                RenderingContext.Current.Rendering.DataSource))
+                {
+                    contentItem = database.GetItem(new Sitecore.Data.ID(
+                    RenderingContext.Current.Rendering.DataSource));
+                }
+            }
+            return View(contentItem);
+        }
+    }
+}
---
+++ Controllers/HelloController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SitecoreDev.Website.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: Hello
+        public ActionResult Index()
+        {
+            try
+            {
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return View("~/Hello/Hello");
+        }
+    }
+}
